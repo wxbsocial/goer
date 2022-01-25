6289437f929123a92610dd4eaf02b2e04f7d373c
wxbsocial <zhanhongfei@wxb.social>
2022-01-25 23:06:03 +0800

    refactor: change type of context key

--- context.go
+++ context.go
@@ -5,19 +5,19 @@ import "context"
 type ContextKey string
 
 var (
-	X_REQUEST_ID     = ContextKey("X-Request-Id")
-	X_CORRELATION_ID = ContextKey("X-Correlation-Id")
-	X_APP_ID         = ContextKey("X-App-Id")
-	X_USER_ID        = ContextKey("X-User-Id")
-	X_USER_NAME      = ContextKey("X-User-Name")
+	X_REQUEST_ID     = "X-Request-Id"
+	X_CORRELATION_ID = "X-Correlation-Id"
+	X_APP_ID         = "X-App-Id"
+	X_USER_ID        = "X-User-Id"
+	X_USER_NAME      = "X-User-Name"
 )
 
 func SetRequestId(ctx context.Context, value string) context.Context {
-	return context.WithValue(ctx, X_REQUEST_ID, value)
+	return context.WithValue(ctx, ContextKey(X_REQUEST_ID), value)
 }
 
 func GetRequestId(ctx context.Context) string {
-	if val, ok := ctx.Value(X_REQUEST_ID).(string); ok {
+	if val, ok := ctx.Value(ContextKey(X_REQUEST_ID)).(string); ok {
 		return val
 	}
 	return ""
@@ -25,44 +25,44 @@ func GetRequestId(ctx context.Context) string {
 }
 
 func SetCorrelationId(ctx context.Context, value string) context.Context {
-	return context.WithValue(ctx, X_CORRELATION_ID, value)
+	return context.WithValue(ctx, ContextKey(X_CORRELATION_ID), value)
 }
 
 func GetCorrelationId(ctx context.Context) string {
-	if val, ok := ctx.Value(X_CORRELATION_ID).(string); ok {
+	if val, ok := ctx.Value(ContextKey(X_CORRELATION_ID)).(string); ok {
 		return val
 	}
 	return ""
 }
 
 func SetAppId(ctx context.Context, value string) context.Context {
-	return context.WithValue(ctx, X_APP_ID, value)
+	return context.WithValue(ctx, ContextKey(X_APP_ID), value)
 }
 
 func GetAppId(ctx context.Context) string {
-	if val, ok := ctx.Value(X_APP_ID).(string); ok {
+	if val, ok := ctx.Value(ContextKey(X_APP_ID)).(string); ok {
 		return val
 	}
 	return ""
 }
 
 func SetUserId(ctx context.Context, value string) context.Context {
-	return context.WithValue(ctx, X_USER_ID, value)
+	return context.WithValue(ctx, ContextKey(X_USER_ID), value)
 }
 
 func GetUserId(ctx context.Context) string {
-	if val, ok := ctx.Value(X_USER_ID).(string); ok {
+	if val, ok := ctx.Value(ContextKey(X_USER_ID)).(string); ok {
 		return val
 	}
 	return ""
 }
 
 func SetUserName(ctx context.Context, value string) context.Context {
-	return context.WithValue(ctx, X_USER_NAME, value)
+	return context.WithValue(ctx, ContextKey(X_USER_NAME), value)
 }
 
 func GetUserName(ctx context.Context) string {
-	if val, ok := ctx.Value(X_USER_NAME).(string); ok {
+	if val, ok := ctx.Value(ContextKey(X_USER_NAME)).(string); ok {
 		return val
 	}
 	return ""
